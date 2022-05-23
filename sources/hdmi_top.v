01f0d448ee790e01a567162fe585fd13932e0413
Zoltan Koren <korenzol.007@gmail.com>
2022-05-23 15:39:28 +0200

    Inst buffer in hdmi top module

--- sources/hdmi_top.v
+++ sources/hdmi_top.v
@@ -143,17 +143,36 @@ hdmi_rx hdmi_rx_0(
    .rx_status(rx_status)
 );
 
+wire [25*8-1:0] kernel_red;
+wire [25*8-1:0] kernel_green;
+wire [25*8-1:0] kernel_blue;
 
 // Loopback
+hdmi_buffer buffer(
+    .clk(clk),
+    .rst(rst),
+    .rx_red(rx_red),
+    .rx_green(rx_green),
+    .rx_blue(rx_blue),
+    .rx_dv(rx_dv),
+    .rx_hs(rx_hs),
+    .rx_vs(rx_vs),
+    .kernel_red(kernel_red),
+    .kernel_green(kernel_green),
+    .kernel_blue(kernel_blue),
+);
 // Replace with image processing block
 wire [7:0] tx_red, tx_green, tx_blue;
 wire tx_dv, tx_hs, tx_vs;
-assign tx_dv    = rx_dv;
-assign tx_hs    = rx_hs;
-assign tx_vs    = rx_vs;
-assign tx_red   = rx_red;
-assign tx_green = rx_green;
-assign tx_blue  = rx_blue;
+//assign tx_dv    = rx_dv;
+//assign tx_hs    = rx_hs;
+//assign tx_vs    = rx_vs;
+//assign tx_red   = rx_red;
+assign tx_red = kernel_red[25*8-1:25*8-8];
+//assign tx_green = rx_green;
+assign tx_green = kernel_green[25*8-1:25*8-8];
+//assign tx_blue  = rx_blue;
+assign tx_blue = kernel_blue[25*8-1:25*8-8];
  
 
 hdmi_tx hdmi_tx_0(
